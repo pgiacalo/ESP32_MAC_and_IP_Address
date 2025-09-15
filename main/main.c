467179d6235eee2cffa60dbc0c3e2c2093fc7fcc
Philip Giacalone <philip_giacalone@yahoo.com>
2025-09-15 09:38:45 -0700

    Change file name to main.c

--- main/main.c
+++ main/main.c
@@ -12,7 +12,7 @@
 
 // WiFi configuration
 #define WIFI_SSID      "Aardvark"
-#define WIFI_PASS      "125125125"
+#define WIFI_PASS      "******"
 #define WIFI_MAXIMUM_RETRY  5
 
 
@@ -131,7 +131,7 @@ void app_main(void)
     
     // Report all information together at the end
     printf("\n=== Device MAC Address ===\n");
-    printf("MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+    printf("ESP32MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     printf("==========================\n");
     
